1eb94905496409bd30080ee2a8c8138a6a95de19
Sagnik Aich <sagnikaich0@gmail.com>
2021-06-17 10:49:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learningForms';
+
+  defaultOption = 'pet'
+
+  onSubmit(form:NgForm){
+
+    console.log(form);
+    // if(form.value.username.trim() == "sagnik" && form.value.email == "sagnikaich0@gmail.com"){
+    //   console.log("Access Granted");
+    // }
+    // else{
+    //   console.log("You are not allowed");
+    // }
+  }
 }
